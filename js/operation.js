5b83109cb341b7aa1e672cb84449360fa11c3339
Grubeman <sebastien.tisseyre0016@orange.fr>
2022-05-06 21:30:49 +0200

    Initial commit

---
+++ js/operation.js
@@ -0,0 +1,27 @@
+class Operation {
+    constructor(parent) {
+        this._uid = uuid.v4()
+        this.parent = parent
+        this.childs = []
+        this.label = undefined
+        this.x = 0
+        this.y = 0
+        this.width = 100
+        this.height = 100
+        this.color = "gray"
+        this.fields = []
+    }
+    get_description_data() {
+        console.error("I should not be here")
+        return
+    }
+
+    static get_modal_data() {
+        return
+    }
+
+    static create(modal) {
+        modal.style.display = "none";
+        return
+    }
+}
